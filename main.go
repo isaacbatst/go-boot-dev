38061f634c088447535b034829429f9c9a9528e2
Isaac Batista <isaacbatst@gmail.com>
2024-02-03 21:02:41 -0300

    4.2: initial code

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+)
+
+type messageToSend struct {
+	message   string
+	sender    user
+	recipient user
+}
+
+type user struct {
+	name   string
+	number int
+}
+
+func canSendMessage(mToSend messageToSend) bool {
+	// ?
+
+	return true
+}
+
+// don't touch below this line
+
+func test(mToSend messageToSend) {
+	fmt.Printf(`sending "%s" from %s (%v) to %s (%v)...`,
+		mToSend.message,
+		mToSend.sender.name,
+		mToSend.sender.number,
+		mToSend.recipient.name,
+		mToSend.recipient.number,
+	)
+	fmt.Println()
+	if canSendMessage(mToSend) {
+		fmt.Println("...sent!")
+	} else {
+		fmt.Println("...can't send message")
+	}
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(messageToSend{
+		message: "you have an appointment tommorow",
+		sender: user{
+			name:   "Brenda Halafax",
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Sally Sue",
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have an event tommorow",
+		sender: user{
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Suzie Sall",
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have an party tommorow",
+		sender: user{
+			name: "Njorn Halafax",
+		},
+		recipient: user{
+			name:   "Becky Sue",
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have a birthday tommorow",
+		sender: user{
+			name:   "Eli Halafax",
+			number: 16545550987,
+		},
+		recipient: user{
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have a birthday tommorow",
+		sender: user{
+			name:   "Jason Bjorn",
+			number: 16545550987,
+		},
+		recipient: user{
+			name: "Jim Bond",
+		},
+	})
+}
