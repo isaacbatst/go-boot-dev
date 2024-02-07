e1f51b2d0219a9929ef4c91c30195f05d01de8f3
Isaac Batista <isaacbatst@gmail.com>
2024-02-07 09:55:24 -0300

    6.2: use sprintf to format message

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func getSMSErrorString(cost float64, recipient string) string {
-	// ?
+	str := fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent", cost, recipient)
+	return str
 }
 
 // don't edit below this line
