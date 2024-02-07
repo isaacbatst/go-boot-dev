a83dbb5b3b479530493ef5e111af928d9038369a
Isaac Batista <isaacbatst@gmail.com>
2024-02-07 10:14:45 -0300

    6.6: use errors.New() to make error

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func divide(x, y float64) (float64, error) {
 	if y == 0 {
-		// ?
+		return 0, errors.New("no dividing by 0")
 	}
 	return x / y, nil
 }
