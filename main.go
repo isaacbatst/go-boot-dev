093762c2d0b9e40b9baa60693f26f57d3fc0cf96
Isaac Batista <isaacbatst@gmail.com>
2024-02-13 11:37:25 -0300

    7.5: solve prime numbers with continue and break

--- main.go
+++ main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func printPrimes(max int) {
-	// ?
+	for n := 2; n <= max; n++ {
+		if n == 2 {
+			fmt.Println(n)
+			continue
+		}
+
+		if n%2 == 0 {
+			continue
+		}
+
+		multipliable := false
+
+		for i := 3; float64(i) <= math.Sqrt(float64(n))+1; i++ {
+			if n%i == 0 {
+				multipliable = true
+				break
+			}
+		}
+
+		if multipliable == false {
+			fmt.Println(n)
+		}
+	}
 }
 
 // don't edit below this line
